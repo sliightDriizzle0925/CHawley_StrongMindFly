2f564cfde3ab720fca00bd7bb34e3cd906a87087
sliightDriizzle <capybara0711@gmail.com>
2025-02-25 14:47:12 -0700

    Fix production binding

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,9 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+
+  config.hosts << "https://pizza-store-broken-water-3534.fly.dev/"
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
