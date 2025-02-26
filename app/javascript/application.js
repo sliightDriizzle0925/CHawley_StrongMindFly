a9d7ac74d9103e5b88d030e32babfbb3a06a5219
sliightDriizzle <capybara0711@gmail.com>
2025-02-26 08:02:53 -0700

    fixing production

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,1 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import Rails from "rails-ujs";
-Rails.start();
-
-import Turbolinks from "turbolinks";
-Turbolinks.start();
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,3 +1,3 @@
 # Pin npm packages by running ./bin/importmap
-
+pin "rails-ujs", to: "rails-ujs.js"
 pin "application"
